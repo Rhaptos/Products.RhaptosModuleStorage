c0518e10aa22e6dfbce54d6cea25a5c6e7455b7f
Josh LaPlace <josh@cnx.rice.edu>
2009-10-27 16:39:53 +0000

    fix module delete sql

--- Products/RhaptosModuleStorage/sql/deleteModule.sql
+++ Products/RhaptosModuleStorage/sql/deleteModule.sql
@@ -2,6 +2,32 @@
 arguments: id:string version:string
 </dtml-comment>
 
+DELETE FROM files WHERE fileid in
+    (SELECT fileid FROM module_files WHERE module_ident IN
+        (SELECT module_ident FROM modules WHERE 
+            <dtml-sqltest id column="moduleid" type="string"> 
+            <dtml-if version>
+            AND
+            <dtml-sqltest version column="version" type="string" optional>
+            </dtml-if>
+    ));
+DELETE FROM module_files WHERE module_ident IN
+    (SELECT module_ident FROM modules WHERE 
+        <dtml-sqltest id column="moduleid" type="string"> 
+        <dtml-if version>
+        AND
+        <dtml-sqltest version column="version" type="string" optional>
+        </dtml-if>
+    );
+DELETE FROM moduletags WHERE module_ident IN
+    (SELECT module_ident FROM modules WHERE 
+        <dtml-sqltest id column="moduleid" type="string"> 
+        <dtml-if version>
+        AND
+        <dtml-sqltest version column="version" type="string" optional>
+        </dtml-if>
+    );
+
 DELETE from modules where 
 <dtml-sqltest id column="moduleid" type="string"> 
 <dtml-if version>
