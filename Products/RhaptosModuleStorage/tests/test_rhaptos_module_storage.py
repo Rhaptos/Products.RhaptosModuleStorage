142e1ffb79a5971868b5508feb03d02889e01a4d
Raj Shah <raj@cnx.rice.edu>
2009-10-29 23:04:19 +0000

    fleshed out module storage tests

--- Products/RhaptosModuleStorage/tests/test_rhaptos_module_storage.py
+++ Products/RhaptosModuleStorage/tests/test_rhaptos_module_storage.py
@@ -26,18 +26,29 @@ $Id: $
 
 import Products.RhaptosModuleStorage
 
+from Products.CMFDefault.Document import Document
+from Products.RhaptosModuleStorage.ModuleVersionFolder import ModuleVersionStorage
+from Products.RhaptosModuleStorage.ModuleView import ModuleFile
+from Products.RhaptosModuleStorage.ModuleView import ModuleView
 from Products.RhaptosTest.base import RhaptosTestCase
 
 
 class TestRhaptosModuleStorage(RhaptosTestCase):
 
     products_to_load_zcml = [('configure.zcml', Products.RhaptosModuleStorage),]
+    products_to_install = ['FSImportTool', 'LinkMapTool', 'ExtZSQL', 'CNXMLDocument', 'RhaptosModuleStorage']
 
     def setUp(self):
         RhaptosTestCase.setUp(self)
+        self.doc = Document('foo bar')
+        self.module_version_storage = ModuleVersionStorage('test_module_version_storage')
+#        self.module_file = ModuleFile()
+#        self.module_view = ModuleView()
 
     def test_module_version_storage(self):
         self.assertEqual(1, 1)
+#        self.assertEqual(self.module_version_storage.countObjects(), 0)
+#        self.assertFalse(self.module_version_storage.hasObject(self.doc.getId()))
 
     def test_module_view(self):
         self.assertEqual(1, 1)
