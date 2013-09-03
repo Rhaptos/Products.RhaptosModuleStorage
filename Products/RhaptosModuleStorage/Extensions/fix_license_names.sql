cd3be4da398b9d05dbb4874c23441d00f746880b
Ross Reedstrom <reedstrm@gmail.com>
2013-09-03 14:22:25 -0500

    fixup existing license names, add one

---
+++ Products/RhaptosModuleStorage/Extensions/fix_license_names.sql
@@ -0,0 +1,2 @@
+update licenses set name = 'Creative Commons '||name||' License'  where name !~ '^Creative Commons';
+INSERT INTO licenses (code, version, name, url) VALUES ('by-nc-sa', '3.0', 'Creative Commons Attribution-NonCommercial-ShareAlike License', 'http://creativecommons.org/licenses/by/3.0/');
