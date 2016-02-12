fef1701cb0c777f040b012aa082a91ac9ee4668f
Ross Reedstrom <reedstrm@gmail.com>
2016-02-12 09:20:21 -0600

    move mimetype from module_files to media_type on files

--- Products/RhaptosModuleStorage/sql/insertFile.sql
+++ Products/RhaptosModuleStorage/sql/insertFile.sql
@@ -1,10 +1,10 @@
 <dtml-comment>
-arguments: file:string
+arguments: file:string string:media_type
 </dtml-comment>
 
 INSERT INTO files 
-  (file) 
+  (file, media_type)
 VALUES 
-  (<dtml-var file>::bytea)
+  (<dtml-var file>::bytea, <dtml-sqlvar media_type type="string">)
 RETURNING fileid
 
