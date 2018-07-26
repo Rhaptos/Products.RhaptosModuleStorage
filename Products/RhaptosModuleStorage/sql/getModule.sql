a518f0af9d6a3e5021b23ba36fd07147d82edcef
Ross Reedstrom <reedstrm@gmail.com>
2018-07-26 17:22:12 -0500

    return keywords as well for getModule

--- Products/RhaptosModuleStorage/sql/getModule.sql
+++ Products/RhaptosModuleStorage/sql/getModule.sql
@@ -8,7 +8,8 @@ SELECT
   m.moduleid AS id, m.portal_type, m.version, m.name, m.created as _created, m.revised as _revised, 
   abstract, m.stateid, m.doctype,l.url AS license, m.module_ident AS ident, m.submitter, m.submitlog, m.print_style,
   p.moduleid AS parent_id, p.version AS parent_version,
-  m.authors as authors, m.licensors as licensors, m.maintainers as maintainers, COALESCE(m.parentauthors,ARRAY(select ''::text where false)) as "parentAuthors", m.language as language, (select '{'||list(''''||roleparam||''':['''||array_to_string(personids,''',''')||''']')||'}' from roles natural join moduleoptionalroles where module_ident=m.module_ident group by module_ident) as _roles, list(tag) as _subject
+  m.authors as authors, m.licensors as licensors, m.maintainers as maintainers, COALESCE(m.parentauthors,ARRAY(select ''::text where false)) as "parentAuthors", m.language as language, (select '{'||list(''''||roleparam||''':['''||array_to_string(personids,''',''')||''']')||'}' from roles natural join moduleoptionalroles where module_ident=m.module_ident group by module_ident) as _roles, list(tag) as _subject,
+  (select list(word) from modulekeywords mk natural left join keywords where mk.module_ident=m.module_ident) as _keywords
 FROM modules m
 NATURAL JOIN abstracts 
 LEFT JOIN modules p on m.parent = p.module_ident
